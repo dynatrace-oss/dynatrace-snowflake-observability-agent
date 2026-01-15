2b7af336476975f33e0235d574071e9dc32601be
Sebastian Kruk <sebastian.kruk@dynatrace.com>
2026-01-15 16:09:41 +0100

    refactor: update deploy script to use named parameters for better clarity and add plugin exclusion handling

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -25,48 +25,86 @@
 
 #
 # This is a script for deploying / updating the whole or part of Dynatrace Snowflake Observability Agent
-# Call as ./deploy.sh "$ENV" "$PARAM1" "$PARAM2" "$PARAM3"
-#%DEV:
-# Double quotes around parameters are necessary if param1 is left empty, and param2 is to be specified
-#%:DEV
+# Call as ./deploy.sh "$ENV" --scope=$SCOPE [--from-version=$VERSION] [--options=$OPTIONS]
 #
 # Args:
-# * ENV      [REQUIRED] - needs to be a environment identifier so that there is a config-$ENV.yml file in the same folder as this script
-# * PARAM1   [OPTIONAL] - can be either
-#              = config         - which will update Dynatrace Snowflake Observability Agent configuration
-#              = apikey         - which will install new Dynatrace Token for Dynatrace Snowflake Observability Agent
-#              = manual         - this will prepare the complete installation script but will not run snowflake-cli to run it
-#              = teardown       - this will remove Dynatrace Snowflake Observability Agent completely
-#              = *              - this will be used to ONLY process scripts PREFIXed with this value, or ALL if none provided
-#%DEV:
-# * PARAM2   [OPTIONAL] - if set to
-#              = no_dep         - the deploy procedure will omit the step of sending script to snowflake
-#              = service_user   - the deploy will use jenkins service user data from environment variables
-#              = *              - the deploy will be executed using data from "conf/config-$1.yml"
-# * PARAM3   [OPTIONAL] - if set to
-#              = skip_confirm   - will not wait for confirmation before deploying to Snowflake
-#%:DEV
+# * ENV            [REQUIRED] - environment identifier (config-$ENV.yml must exist)
+# * --scope        [OPTIONAL] - deployment scope (default: all):
+#                               init, setup, plugins, config, agents, apikey, all, teardown, upgrade, or file_part
+# * --from-version [OPTIONAL] - version number for upgrade scope (required if scope=upgrade)
+# * --options      [OPTIONAL] - comma-separated: manual, service_user, skip_confirm, no_dep
 #
 
 ENV=$1
-PARAM=$2
+shift
+
+# Parse arguments
+SCOPE="all"
+FROM_VERSION=""
+OPTIONS_STR=""
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --scope=*)
+            SCOPE="${1#*=}"
+            shift
+            ;;
+        --from-version=*)
+            FROM_VERSION="${1#*=}"
+            shift
+            ;;
+        --options=*)
+            OPTIONS_STR="${1#*=}"
+            shift
+            ;;
+        *)
+            echo "Unknown parameter: $1"
+            exit 1
+            ;;
+    esac
+done
+
+# Parse options into array
+IFS=',' read -ra OPTIONS <<< "$OPTIONS_STR"
+
+# Check if option is present
+has_option() {
+    local opt=$1
+    for item in "${OPTIONS[@]}"; do
+        [[ "$item" == "$opt" ]] && return 0
+    done
+    return 1
+}
+
 CWD=$(dirname "$0")
 
-if [ "$PARAM" == "manual" ]; then
+if has_option "manual"; then
     "$CWD/setup.sh"
 else
     "$CWD/setup.sh" $ENV
 fi
 
-echo "Deploying Dynatrace Snowflake Observability Agent with [$ENV] configuration, using PARAM=[$PARAM]"
+echo "Deploying Dynatrace Snowflake Observability Agent with [$ENV] configuration, using SCOPE=[$SCOPE]"
 
 if [ "$ENV" == '' ]; then
     echo "Needs environment name to proceed"
     exit 1
 fi
+
+if [ "$SCOPE" == '' ]; then
+    echo "Needs --scope parameter to proceed"
+    exit 1
+fi
+
+if [ "$SCOPE" == "upgrade" ] && [ "$FROM_VERSION" == '' ]; then
+    echo "--from-version parameter is required when scope=upgrade"
+    exit 1
+fi
+# FIXME implement upgrade procedure
+
 #%DEV:
 # we only need to check DTAGENT_TOKEN if we are deploying through Jenkins
-if [ "$3" == 'service_user' ] && [ -z "$DTAGENT_TOKEN" ]; then
+if has_option "service_user" && [ -z "$DTAGENT_TOKEN" ]; then
     echo "Environment variable DTAGENT_TOKEN is not defined"
     exit 1
 fi
@@ -79,7 +117,8 @@ if [ ! -f "$CONFIG_FILE" ]; then
     echo "There is no configuration file [conf/config-$ENV.yml]"
     #%DEV:
     # we could just exit if config file doesn't exist and we either do not want to deploy at all or want to deploy through jenkins
-    if [ "$3" != 'no_dep' ] && [ "$3" != 'service_user' ]; then
+
+    if ! has_option "no_dep" && ! has_option "service_user"; then
         #%:DEV
         exit 1
         #%DEV:
@@ -97,7 +136,7 @@ DEPLOYMENT_ENV="$($CWD/get_config_key.sh core.deployment_environment)"
 CONNECTION_ENV="${DEPLOYMENT_ENV,,}" # convert to lower case
 NOW_TS=$(date '+%Y%m%d-%H%M%S')
 
-if [ "$PARAM" == "manual" ]; then
+if has_option "manual"; then
     INSTALL_SCRIPT_SQL="dsoa-deploy-script-${DEPLOYMENT_ENV}-${NOW_TS}.sql"
 else
     INSTALL_SCRIPT_SQL=$(mktemp -p build)
@@ -105,12 +144,52 @@ else
     trap " rm -f ${INSTALL_SCRIPT_SQL} " EXIT
 fi
 
+# Get list of plugins to exclude
+EXCLUDED_PLUGINS=$($CWD/list_plugin_to_exclude.sh)
+
+# Filter function to remove disabled plugin code
+filter_plugin_code() {
+    local input_file=$1
+    local output_file=$2
+
+    if [ -z "$EXCLUDED_PLUGINS" ]; then
+        cat "$input_file" > "$output_file"
+        return
+    fi
+
+    local temp_file=$(mktemp)
+    cp "$input_file" "$temp_file"
+
+    for plugin_name in $EXCLUDED_PLUGINS; do
+        awk -v plugin="$plugin_name" '
+            BEGIN { active=1; }
+            /^([#]|[-]{2})[%]PLUGIN:/ {
+                if (index($0, plugin":") > 0) active=0; #FIXME: we should be more precise here
+            }
+            { if (active==1) print $0; }
+            /^([#]|[-]{2})[%][:]PLUGIN:/ {
+                if (index($0, plugin) > 0) active=1; #FIXME: we should be more precise here
+            }
+        ' "$temp_file" > "$output_file"
+        cp "$output_file" "$temp_file"
+    done
+
+    rm "$temp_file"
+}
+
 # preparing one big deployment script
-$CWD/prepare_deploy_script.sh "${INSTALL_SCRIPT_SQL}" "${ENV}" "${PARAM}"
+$CWD/prepare_deploy_script.sh "${INSTALL_SCRIPT_SQL}" "${ENV}" "${SCOPE}" "${FROM_VERSION}"
 
-if [ -s "$INSTALL_SCRIPT_SQL" ] && [ "$PARAM" != "manual" ]; then
+# Apply plugin filtering for non-special scopes
+if [ "$SCOPE" != "apikey" ] && [ "$SCOPE" != "teardown" ]; then
+    FILTERED_SQL=$(mktemp -p build)
+    filter_plugin_code "${INSTALL_SCRIPT_SQL}" "${FILTERED_SQL}"
+    mv "${FILTERED_SQL}" "${INSTALL_SCRIPT_SQL}"
+fi
 
-    if [ "${PARAM}" == "service_user" ]; then
+if [ -s "$INSTALL_SCRIPT_SQL" ] && ! has_option "manual"; then
+
+    if has_option "service_user"; then
         # added for Jenkins to be able to skip this step, as it will never find the config file
         # this is taken care of in the update_config.py, and config_file doesn't exist necessary data is taken from environment variables
         SNOWFLAKE_ACCOUNT_NAME=${SNOWFLAKE_ACC_NAME}
@@ -129,26 +208,25 @@ if [ -s "$INSTALL_SCRIPT_SQL" ] && [ "$PARAM" != "manual" ]; then
 
     echo -e "Deploying to Snowflake with the snow_agent_$CONNECTION_ENV connection profile and as the $DEPLOYMENT_ENV deployment environment\n"
 
-    if [ "$4" != 'skip_confirm' ]; then
+    if ! has_option "skip_confirm"; then
         read -p "Press Enter if you wish to continue deployment with script above or Ctrl+C to exit" </dev/tty
     fi
 
-    if [ "$PARAM" != 'no_dep' ]; then
-        if ! $CWD/send_bizevent.sh "${PARAM}" "STARTED" "${DEPLOYMENT_ID}"; then
+    if ! has_option "no_dep"; then
+        if ! $CWD/send_bizevent.sh "${SCOPE}" "STARTED" "${DEPLOYMENT_ID}"; then
             echo "Encountered issues when sending deployment bizevent, proceeding..."
         fi
     fi
-
     #%DEV:
-    if [ "$3" != 'no_dep' ] && [ "$3" != 'service_user' ]; then
+
+    if ! has_option "no_dep" && ! has_option "service_user"; then
         #%:DEV
         pushd build
         snow sql --connection "snow_agent_$CONNECTION_ENV" \
             --filename "$(basename ${INSTALL_SCRIPT_SQL})"
         popd
         #%DEV:
-    elif [ "$3" == 'service_user' ]; then
-
+    elif has_option "service_user"; then
         pushd build
         snow sql --temporary-connection \
             --account ${SNOWFLAKE_ACCOUNT_NAME} \
@@ -162,14 +240,14 @@ if [ -s "$INSTALL_SCRIPT_SQL" ] && [ "$PARAM" != "manual" ]; then
 
     rm ${INSTALL_SCRIPT_SQL}
 
-elif [ "$PARAM" == 'manual' ]; then
+elif has_option "manual"; then
     echo "Skipping automated deployment"
 else
-    echo "No scripts matching requested deploy filter: $PARAM"
+    echo "No scripts matching requested deploy filter: $SCOPE"
 fi
 
-if [ "$PARAM" != 'no_dep' ]; then
-    if ! $CWD/send_bizevent.sh "${PARAM}" "FINISHED" "${DEPLOYMENT_ID}"; then
+if ! has_option "no_dep"; then
+    if ! $CWD/send_bizevent.sh "${SCOPE}" "FINISHED" "${DEPLOYMENT_ID}"; then
         echo "Encountered issues when sending deployment bizevent, proceeding..."
     fi
 fi
